fbe4bce246af429188d8d731e04fbe18f4b95adb
Alex Sereno <2569805+alex@users.noreply.github.com>
2020-12-16 18:35:05 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alexander Sereno on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
